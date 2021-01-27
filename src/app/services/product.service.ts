2b66328d67d4aaecb71f8889ce04143790393f79
benhsieh-dev <bqh5026@gmail.com>
2021-01-27 17:15:42 -0500

    Add searchUrl to getProductList() in prodct.service.ts. Update httpClient.get to use searchUrl

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -10,13 +10,14 @@ import { map } from 'rxjs/operators';
 export class ProductService {
 
   private baseUrl = 'http://localhost:8080/api/products'; 
-
+  
   constructor(private httpClient: HttpClient) { }
-
+  
   getProductList(theCategoryId: number): Observable<Product[]> {
 
-    // Need to update the backend to use this method
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`; 
+
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
