a9c4667e30073c1bf29847608433ff83b0740b12
benhsieh-dev <bqh5026@gmail.com>
2021-01-28 20:00:05 -0500

    Add product category class and product category component

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,11 +3,20 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators'; 
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+
+  private categoryUrl = 'http://localhost:8080/api/product-category';
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
 
   private baseUrl = 'http://localhost:8080/api/products'; 
   
@@ -26,5 +35,11 @@ export class ProductService {
 interface GetResponse {
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[]; 
   }
 }
