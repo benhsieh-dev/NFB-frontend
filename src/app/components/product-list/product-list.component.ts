18a7b721344c13044e6473e4a703823376eb80e7
benhsieh-dev <bqh5026@gmail.com>
2021-02-03 21:45:01 -0500

    Add cart item class

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,103 +1,116 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
-
 export class ProductListComponent implements OnInit {
-
-  products: Product[] = []; 
+  products: Product[] = [];
   currentCategoryId: number = 1;
   previousCategoryId: number = 1;
-  searchMode: boolean = false; 
+  searchMode: boolean = false;
 
   thePageNumber: number = 1;
-  thePageSize: number = 10; 
-  theTotalElements: number = 0; 
+  thePageSize: number = 10;
+  theTotalElements: number = 0;
 
-  previousKeyword: string = null; 
+  previousKeyword: string = null;
 
-  constructor(private productService: ProductService, 
-              private route: ActivatedRoute) { }
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe(() => {
-      this.listProducts(); 
-    })
+      this.listProducts();
+    });
     this.listProducts();
   }
 
   listProducts() {
-    this.searchMode = this.route.snapshot.paramMap.has('keyword'); 
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
     if (this.searchMode) {
-      this.handleSearchProducts();  
+      this.handleSearchProducts();
     } else {
-      this.handleListProducts(); 
+      this.handleListProducts();
     }
   }
 
   handleSearchProducts() {
-     const theKeyword: string = this.route.snapshot.paramMap.get('keyword'); 
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
 
-     if (this.previousKeyword !== theKeyword) {
-       this.thePageNumber = 1; 
-     }
+    if (this.previousKeyword !== theKeyword) {
+      this.thePageNumber = 1;
+    }
 
-     this.previousKeyword = theKeyword;
+    this.previousKeyword = theKeyword;
 
-     console.log(`keyword=${theKeyword}, thePageNumber=${this.thePageNumber}`);
+    console.log(`keyword=${theKeyword}, thePageNumber=${this.thePageNumber}`);
 
-    this.productService.searchProductsPaginate(
-      this.thePageNumber - 1,
-      this.thePageSize,
-      theKeyword).subscribe(this.processResult()); 
-
+    this.productService
+      .searchProductsPaginate(
+        this.thePageNumber - 1,
+        this.thePageSize,
+        theKeyword
+      )
+      .subscribe(this.processResult());
   }
 
   handleListProducts() {
-
-    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id'); 
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
     if (hasCategoryId) {
-      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');  
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
     } else {
-      this.currentCategoryId = 1; 
+      this.currentCategoryId = 1;
     }
 
     if (this.previousCategoryId != this.currentCategoryId) {
-      this.thePageNumber = 1; 
+      this.thePageNumber = 1;
     }
 
-    this.previousCategoryId = this.currentCategoryId; 
+    this.previousCategoryId = this.currentCategoryId;
 
-    console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
+    console.log(
+      `currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`
+    );
 
-    this.productService.getProductListPaginate(this.thePageNumber - 1, this.thePageSize, this.currentCategoryId).subscribe(this.processResult());
-
+    this.productService
+      .getProductListPaginate(
+        this.thePageNumber - 1,
+        this.thePageSize,
+        this.currentCategoryId
+      )
+      .subscribe(this.processResult());
   }
 
   private processResult() {
-    return data => {
+    return (data) => {
       this.products = data._embedded.products;
       this.thePageNumber = data.page.number + 1;
       this.thePageSize = data.page.size;
-      this.theTotalElements = data.page.totalElements; 
-    }
-  }
-
-  addToCart(theProduct: Product) {
-    console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`); 
+      this.theTotalElements = data.page.totalElements;
+    };
   }
 
   updatePageSize(pageSize: number) {
     this.thePageSize = pageSize;
     this.thePageNumber = 1;
-    this.listProducts(); 
+    this.listProducts();
   }
-  
+
+  addToCart(theProduct: Product) {
+    console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+
+    const theCartItem = new CartItem(theProduct);
+    this.cartService.addToCart(theCartItem); 
+  }
 }
---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { CartItem } from '../common/cart-item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItems: CartItem[] = [];
+  
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+  constructor() { }
+
+  addToCart(theCartItem: CartItem) {
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem = undefined; 
+    if (this.cartItems.length > 0) {
+      for (let tempCartItem of this.cartItems) {
+        if (tempCartItem.id === theCartItem.id) {
+          existingCartItem = tempCartItem;
+          break;
+        }
+      }
+
+      alreadyExistsInCart = (existingCartItem != undefined)
+
+      if(alreadyExistsInCart) {
+        existingCartItem.quantity++;
+      } else {
+        this.cartItems.push(theCartItem); 
+      }
+
+      this.computeCartTotal(); 
+
+    }
+
+  } 
+
+  computeCartTotal() {
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    for (let currrentCartItem of this.cartItems) {
+      totalPriceValue += currrentCartItem.quantity * currrentCartItem.unitPrice;
+      totalQuantityValue += currrentCartItem.quantity;
+    }
+
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+    
+  }
+}
