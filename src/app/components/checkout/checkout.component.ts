161bcd6a4c8413d6dec026f32162c9d025d27724
benhsieh-dev <bqh5026@gmail.com>
2021-02-08 16:40:58 -0500

    Autofill feature for billing address if same as shipping address

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -4,16 +4,14 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit {
-
   checkoutFormGroup: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: [''],
@@ -45,18 +43,26 @@ export class CheckoutComponent implements OnInit {
         expirationMonth: [''],
         expirationYear: [''],
       }),
-
     });
-
- 
   }
 
   onSubmit() {
-    console.log("Handling the submit button");
+    console.log('Handling the submit button');
     // console.log(this.checkoutFormGroup.get('customer').value);
     // console.log("The email addres is " + this.checkoutFormGroup.get('customer').value.email);
 
-    console.log("The country is " + this.checkoutFormGroup.get('country').value);
+    console.log(
+      'The country is ' + this.checkoutFormGroup.get('country').value
+    );
+  }
+
+  copyShippingAddressToBillingAddress(event) {
+    if (event.target.checked) {
+      this.checkoutFormGroup.controls.billingAddress
+      .setValue(this.checkoutFormGroup.controls.shippingAddress.value);
+    } else {
+      this.checkoutFormGroup.controls.billingAddress.reset(); 
+    }
   }
 
 }
