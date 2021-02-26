2cadb1749da44f82e70975fbcc652f0a8e0d1449
benhsieh-dev <bqh5026@gmail.com>
2021-02-26 15:57:11 -0500

    Fix cors policy issue in my-app-config.ts by correcting the spelling from oath2 to oauth2

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,7 +1,7 @@
 export default {
   oidc: {
     clientId: '0oa9tz60nboljykR05d6',
-    issuer: 'https://dev-46448354.okta.com/oath2/default',
+    issuer: 'https://dev-46448354.okta.com/oauth2/default',
     redirectUri: 'http://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
