3ac24ff0d6e01dacadd02f1e4b6a9647320ceb2e
benhsieh-dev <bqh5026@gmail.com>
2021-03-17 22:34:53 -0400

    Test Heroku Okta redirect

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -8,7 +8,8 @@ export default {
   oidc: {
     clientId: '0oac1kw4vZ9vG4i0m5d6',
     issuer: 'https://dev-6013813.okta.com/oauth2/default',
-    redirectUri: 'https://newfullybakery.herokuapp.com/login/callback',
+    // redirectUri: 'https://newfullybakery.herokuapp.com/login/callback',
+    redirectUri: 'https://newfullybakery.herokuapp.com/login/oauth2/code/okta',
     scopes: ['openid', 'profile', 'email'],
   },
 };
