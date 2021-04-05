c66faf1933995a4002d47bba55b30018dc5c3355
benhsieh-dev <bqh5026@gmail.com>
2021-04-04 23:13:46 -0400

    Add comments to clarify codes

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,10 +1,13 @@
 export default {
+  // localhost Okta credentials
   // oidc: {
   //   clientId: '0oa9tz60nboljykR05d6',
   //   issuer: 'https://dev-46448354.okta.com/oauth2/default',
   //   redirectUri: 'http://localhost:4200/login/callback',
   //   scopes: ['openid', 'profile', 'email'],
   // },
+
+  // Heroku Okta credentials
   oidc: {
     clientId: '0oac1kw4vZ9vG4i0m5d6',
     issuer: 'https://dev-6013813.okta.com/oauth2/default',
