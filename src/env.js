1cda566e40dc515b14cd592e355cdc60245da4b0
Ben Hsieh <bqh5026@gmail.com>
2021-08-17 15:55:40 -0500

    Test Okta production configurations

--- src/env.js
+++ src/env.js
@@ -2,4 +2,7 @@
   window.__env = window.__env || {};
   // window.__env.backEndBaseUrl = "http://localhost:8080";
   window.__env.backEndBaseUrl = "https://newfullybakery-backend.herokuapp.com";
+  window.__env.OktaClientId = '0oac1kw4vZ9vG4i0m5d6';
+  window.__env.OktaOidcIssuer = 'https://dev-6013813.okta.com/oauth2/default'; 
+  window.__env.OktaOidcRedirectUri = 'https://newfullybakery.herokuapp.com/login/oauth2/code/okta';
 })(this);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,13 @@ export const environment = {
   baseUrl: 'https://newfullybakery.herokuapp.com',
 
   //Heroku NFB backend URL
-  backEndBaseUrl: 'https://newfullybakery-backend.herokuapp.com'
+  backEndBaseUrl: 'https://newfullybakery-backend.herokuapp.com',
 
   // hostURL: 'https://newfullybakery-backend.herokuapp.com',
   // envName: 'prod'
+
+  // Okta
+  OktaClientId: '0oac1kw4vZ9vG4i0m5d6',
+  OktaOidcIssuer: 'https://dev-6013813.okta.com/oauth2/default', 
+  OktaOidcRedirectUri: 'https://newfullybakery.herokuapp.com/login/oauth2/code/okta'
 };
