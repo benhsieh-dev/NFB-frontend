95bbb256c2bf10f7ba848cbef81f963282429582
Ben Hsieh <bqh5026@gmail.com>
2021-08-26 15:20:51 -0500

    Edit comments in configuration file

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -6,23 +6,7 @@ const OktaClientId = environment.OktaClientId
 
 export default {
 
-  // localhost Okta credentials
-  // oidc: {
-  //   clientId: '0oa9tz60nboljykR05d6',
-  //   issuer: 'https://dev-46448354.okta.com/oauth2/default',
-  //   redirectUri: 'http://localhost:4200/login/callback',
-  //   scopes: ['openid', 'profile', 'email'],
-  // },
-
-  // Heroku Okta credentials
-  // oidc: {
-  //   clientId: '0oac1kw4vZ9vG4i0m5d6',
-  //   issuer: 'https://dev-6013813.okta.com/oauth2/default',
-  //   redirectUri: 'https://newfullybakery.herokuapp.com/login/oauth2/code/okta',
-  //   scopes: ['openid', 'profile', 'email'],
-  // },
-
-  // Okta environment specific configurations
+  // Dynamic environment configurations for localhost and Okta
    oidc: {
     clientId: `${OktaClientId}`,
     issuer: `${OktaOidcIssuer}`,
